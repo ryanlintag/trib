8d5fe3e5d0f197e8aca1b91f401c772e355efbf7
Ryan Lintag <77764315+ryanlintag-who@users.noreply.github.com>
2023-12-05 22:10:03 +0800

    Initial code

---
+++ RazorClassLibrary/Services/UsersService.cs
@@ -0,0 +1,16 @@
+﻿using RazorClassLibrary.ViewModels;
+
+namespace RazorClassLibrary.Services
+{
+    public interface IUserService
+    {
+        List<GetUserListItemViewModel> GetUsers();
+    }
+    public class UsersService : IUserService
+    {
+        public List<GetUserListItemViewModel> GetUsers()
+        {
+            return new List<GetUserListItemViewModel>();
+        }
+    }
+}
