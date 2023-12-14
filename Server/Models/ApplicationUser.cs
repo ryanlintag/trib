8eceef25f33f8fa048857482cc357c4b5b4eb1d6
Ryan Lintag <77764315+ryanlintag-who@users.noreply.github.com>
2023-12-14 22:21:11 +0800

    Blazor App connected to Entra ID

---
+++ Server/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Blazor8.Server.Models
+{
+    public partial class ApplicationUser
+    {
+        public string Name { get; set; }
+    }
+}
