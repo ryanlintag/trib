869925b58fc286234655929329bb700673c5ba49
Ryan Lintag <ryanlintag@yahoo.com>
2025-02-23 23:24:49 +0800

    Identity Token Generation

---
+++ trib/src/presentation/identity/Program.cs
@@ -0,0 +1,24 @@
+using identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSingleton< TokenGenerator>();
+
+var app = builder.Build();
+
+app.MapPost("/signin", (LoginInputModel model, TokenGenerator tokenGenerator) => {
+
+    return new 
+    {
+        access_token = tokenGenerator.GenerateToken(Guid.NewGuid(), model.email)
+    };
+
+});
+
+
+app.Run();
+
+
+public sealed record LoginInputModel(string email, string password);
