572df6e647d7d87bd9c08dc4c58bf0d4a86840ab
Ryan Lintag <ryanlintag@yahoo.com>
2025-04-01 23:23:06 +0800

    Removed Presentation Layer

--- trib/src/presentation/TribApp/TribApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TribApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
